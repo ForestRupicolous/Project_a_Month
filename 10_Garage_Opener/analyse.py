2742935d6beaeafa62ad76814d3c4b87d9f982eb
Martin Korinek <m.ko@inode.at>
2017-10-01 21:04:13 +0200

    Update analyzer

--- 10_Garage_Opener/analyse.py
+++ 10_Garage_Opener/analyse.py
@@ -1,24 +1,32 @@
 import pandas as pd
 import numpy as np
 
-df = pd.read_csv('C:/Projekte/1_Project_a_Month/10_Garage_Opener/NewFile2.csv', header=0)
+df = pd.read_csv('C:/Projekte/1_Project_a_Month/10_Garage_Opener/NewFile3.csv', header=1)
 print df
-df['Logic'] = df['Volt']>5.0
+df = df.round(6)
+Logic_Level = df['Volt'].mean()
+df['Logic'] = df['Volt']>Logic_Level
 print df
 df['Switching'] = np.where(df['Logic'] != df['Logic'].shift(1),df['Second'],np.nan)
 df['Timing'] = df['Switching'].fillna(method='pad')
 df['Duration'] = df['Timing'].diff()
 ds = df[df['Duration']!=0]
 conditions = [
-	(ds['Duration'].shift(1).between(0.000090, 0.000093, inclusive=True)) & 
-		(ds['Duration'].between(0.000350, 0.000354, inclusive=True)),
-	(ds['Duration'].shift(1).between(0.000090, 0.000093, inclusive=True)) & 
-		(ds['Duration'].between(0.000173, 0.000176, inclusive=True)),
-	(ds['Duration'].shift(1).between(0.000267, 0.000270, inclusive=True)) & 
-		(ds['Duration'].between(0.000350, 0.000354, inclusive=True)),
-	(ds['Duration'].shift(1).between(0.000267, 0.000270, inclusive=True)) & 
-		(ds['Duration'].between(0.000173, 0.000176, inclusive=True))]
-choices = [1, 0, 1, 0]
-ds['Real_Bits'] = np.select(conditions,choices,default = np.nan)
-print ds
-ds.to_csv('C:/Projekte/1_Project_a_Month/10_Garage_Opener/msrmt_opt.csv', index = False)
+	(ds['Duration'].shift(1).between(0.000088, 0.000096, inclusive=True)) & 
+		(ds['Duration'].between(0.000340, 0.000360, inclusive=True)),
+	(ds['Duration'].shift(1).between(0.000088, 0.000096, inclusive=True)) & 
+		(ds['Duration'].between(0.000170, 0.000180, inclusive=True)),
+	(ds['Duration'].shift(1).between(0.000260, 0.000275, inclusive=True)) & 
+		(ds['Duration'].between(0.000340, 0.000360, inclusive=True)),
+	(ds['Duration'].shift(1).between(0.000260, 0.000275, inclusive=True)) & 
+		(ds['Duration'].between(0.000170, 0.000180, inclusive=True)),
+    (ds['Duration'].shift(1).between(0.000088, 0.000096, inclusive=True)) & 
+		(ds['Duration']>0.005)]
+choices = [1, 0, 1, 0, -1]
+ds['Real_Bits'] = np.select(conditions,choices,default = 'X')
+dx = ds[ds['Real_Bits']!='X']
+print dx
+print Logic_Level
+dx.to_csv('C:/Projekte/1_Project_a_Month/10_Garage_Opener/msrmt_opt.csv', index = False)
+
+#TODO:abfragen checken!
