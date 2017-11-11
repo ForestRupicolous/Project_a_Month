b33f26cfae4f8aa3aa5235d98a408086f8a7d6ba
Martin Korinek <m.ko@inode.at>
2017-11-11 15:27:17 +0100

    First try with neopixel example

--- 11_Mini_USB_Blinky/digispark_usb_blinky/digispark_usb_blinky.ino
+++ 11_Mini_USB_Blinky/digispark_usb_blinky/digispark_usb_blinky.ino
@@ -1,16 +1,30 @@
-// the setup routine runs once when you press reset:
-void setup() {                
-  // initialize the digital pin as an output.
- // pinMode(0, OUTPUT); //LED on Model B
-  pinMode(1, OUTPUT); //LED on Model A   
+// NeoPixel Ring simple sketch (c) 2013 Shae Erisson
+// released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
+#include <Adafruit_NeoPixel.h>
+
+// Which pin on the Arduino is connected to the NeoPixels?
+#define PIN            0
+
+// How many NeoPixels are attached to the Arduino?
+#define NUMPIXELS      1
+
+// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
+// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
+// example for more information on possible values.
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_RGB + NEO_KHZ800);
+
+int delayval = 500; // delay for half a second
+
+void setup() {
+  pixels.begin(); // This initializes the NeoPixel library.
 }
 
-// the loop routine runs over and over again forever:
 void loop() {
- // digitalWrite(0, HIGH);   // turn the LED on (HIGH is the voltage level)
-  digitalWrite(1, HIGH);
-  delay(1000);               // wait for a second
- // digitalWrite(0, LOW);    // turn the LED off by making the voltage LOW
-  digitalWrite(1, LOW); 
-  delay(1000);               // wait for a second
+  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+  for(int i=0;i<NUMPIXELS;i++){
+    // pixels.Color takes RGB values, from 0,0,0 up to 255,255,255
+    pixels.setPixelColor(i, pixels.Color(0,150,0)); // Moderately bright green color.
+    pixels.show(); // This sends the updated pixel color to the hardware.
+    delay(delayval); // Delay for a period of time (in milliseconds).
+  }
 }
